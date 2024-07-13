55d71b03cd9cfb28257b52620fdf907251fe446a
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-13 13:13:36 +0300

    Linking Between Routes With <Link /> and <NavLink />

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <h1>World Wise</h1>;
+  return (
+    <div>
+      <PageNav />
+      <h1>World Wise</h1>
+    </div>
+  );
 }
 
 export default Homepage;
