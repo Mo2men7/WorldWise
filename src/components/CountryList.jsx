8320faf38e9e4273802e17cf8ca7e0633b649123
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-15 03:11:21 +0300

    Consuming the CitiesContext

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,8 +3,10 @@ import CountryItem from "./CountryItem";
 import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city to get the country here" />;
